d23b8e75220813fc94992b9397481b0656bf89c5
Ben Hall <Benjamin2442@hotmail.co.uk>
2018-07-10 21:32:06 +0100

    contact form working and sending email address to mongo database

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express    = require("express"),
-      app        = express(),
-      bodyParser = require('body-parser'),
-      mongoose   = require("mongoose");
+const express     = require("express"),
+      app         = express(),
+      bodyParser  = require('body-parser'),
+      mongoose    = require("mongoose"),
+			nodemailer  = require("nodemailer");
 
 app.use(express.static('public'));
 mongoose.connect("mongodb://localhost/TShair");
@@ -12,12 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // MONGOOSE SCHEMA
 //===============================================
 
-var contactQuerySchema = new mongoose.Schema({
-    queryEmail: String,
-    queryText: String
+var contactEmailSchema = new mongoose.Schema({
+    queryEmail: String
 });
 
-var Query = mongoose.model("TShairQuery", contactQuerySchema);
+var contactEmail = mongoose.model("TShairEmail", contactEmailSchema);
 
 
 //===============================================
@@ -33,16 +33,49 @@ app.get("/contact", function(req, res){
 });
 
 app.post("/contact", function(req, res){
+
+		// get form info that was submitted
 		let queryEmail = req.body.queryEmail;
     let queryText = req.body.queryText;
-    let newQuery = {queryEmail:queryEmail,queryText:queryText};
-    Query.create(newQuery, function(err){
+    let newEmail = {queryEmail:queryEmail};
+
+		// check email address isnt already in teh database
+		// add email address to data base
+		contactEmail.create(newEmail, function(err){
         if(err){
             console.log(err);
-        }else{
-            res.redirect("contact");
         }
     });
+
+		//send email to desired email address
+		let transporter = nodemailer.createTransport({
+			service: 'Gmail',
+			secure: false,
+			port: 25,
+			auth: {
+				user: "Benjamin.hall2442@gmail.com",
+				pass: "Fqks5c2442"
+			},
+			tls: {
+				rejectUnauthorized: false
+			}
+		});
+		let HelperOptions = {
+			from: '"Ben Hall" <Benjamin.hall2442@gmail.com>',
+			to: 'Benjamin.hall2442@gmail.com',
+			subject: queryEmail,
+			text: "<"+queryEmail+">"+ queryText
+		};
+		transporter.sendMail(HelperOptions,(error, info) => {
+			if(error){
+			 return	console.log(error);
+			}
+			console.log("The message was sent!");
+			console.log(info);
+		});
+
+		// redirect to contact page with "message sent alert"
+		res.redirect('contact');
 });
 
 app.get("/treatments", function(req, res){
