722efc607be6b1d4c0356d3c5413c80ed6f42d82
Ben Hall <Benjamin2442@hotmail.co.uk>
2018-07-11 18:27:07 +0100

    mailchimp working and mongo removed for now

--- app.js
+++ app.js
@@ -1,68 +1,78 @@
 const express     = require("express"),
       app         = express(),
       bodyParser  = require('body-parser'),
-      mongoose    = require("mongoose"),
+      // mongoose    = require("mongoose"),
 			nodemailer  = require("nodemailer"),
 			request     = require("request");
 
 app.use(express.static('public'));
-mongoose.connect("mongodb://localhost/TShair");
+// mongoose.connect("mongodb://localhost/TShair"); NOT IN USE AT THE MOMENT!!!
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }))
 
 //===============================================
-// MONGOOSE SCHEMA
+// MONGOOSE SCHEMA    NOT IN USE AT THE MOMENT!!!! USING MAILCHIMP INSTEAD
 //===============================================
 
-var queryContactEmailSchema = new mongoose.Schema({
-    email: String
-});
+// var queryContactEmailSchema = new mongoose.Schema({
+//     email: String
+// });
+//
+// var queryContactEmail = mongoose.model("TShairEmail", queryContactEmailSchema);
 
-var queryContactEmail = mongoose.model("TShairEmail", queryContactEmailSchema);
+//===============================================
+// EMAIL TO MAILCHIMP FUNCTION
+//===============================================
+
+function addEmailToMailchimp(email){
+	var options = { method: 'POST',
+  url: 'https://us18.api.mailchimp.com/3.0/lists/c13587663f/members',
+  headers:
+   { 'Postman-Token': 'c2acb848-e135-4fcf-bcf9-1c249281eba6',
+     'Cache-Control': 'no-cache',
+     Authorization: 'Basic YW55c3RyaW5nOmQzOTBkZTI3NmEwZjMxNzA1MGQ2ODk5MDQyNDljNWNlLXVzMTg=',
+     'Content-Type': 'application/json' },
+  body: { email_address: email, status: 'subscribed' },
+  json: true };
+
+	request(options, function (error, response, body) {
+	  if (error) throw new Error(error);
+
+		  console.log(body);
+	});
+}
 
 //===============================================
 // ROUTES
 //===============================================
 
+// RENDERS LANDING PAGE
 app.get("/", function(req, res){
     res.render("36hair");
 });
 
+// POST REQUEST FOR SENDING EMAIL TO MAILCHIMP LIST
 app.post("/", function(req, res){
 
-	// get form info that was submitted
+	// get form info that was submitted & submit to mailchimp
 	let email = req.body.mailingEmail;
-	let newEmail = {email:email};
-
-	// add email address to data base
-	queryContactEmail.create(newEmail, function(err){
-			if(err){
-					console.log(err);
-			}
-	});
+	addEmailToMailchimp(email);
 
 	// redirect to contact page with "message sent alert"
 	res.redirect('/');
 });
 
+// RENDERS CONTACT PAGE
 app.get("/contact", function(req, res){
 	res.render("contact");
 });
 
+// POST REQUEST FOR SENDING EMAIL TO GMAIL FROM CONTACT FORM
 app.post("/contact", function(req, res){
 
-		// get form info that was submitted
+		// get form info that was submitted to send as an email
 		let email = req.body.queryEmail;
     let queryText = req.body.queryText;
-    let newEmail = {email:email};
-
-		// check email address isnt already in the database
-		// add email address to data base
-		queryContactEmail.create(newEmail, function(err){
-        if(err){
-            console.log(err);
-        }
-    });
 
 		//send email to desired email address
 		let transporter = nodemailer.createTransport({
@@ -95,6 +105,7 @@ app.post("/contact", function(req, res){
 		res.redirect('contact');
 });
 
+// RENDERS TREATMENTS PAGE
 app.get("/treatments", function(req, res){
 	res.render("treatments");
 });
