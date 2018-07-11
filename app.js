94a1527999bc260bb05291435658d07c03d70360
Ben Hall <Benjamin2442@hotmail.co.uk>
2018-07-11 17:01:12 +0100

    changing from mongodb to mailchimp api key

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express     = require("express"),
       app         = express(),
       bodyParser  = require('body-parser'),
       mongoose    = require("mongoose"),
-			nodemailer  = require("nodemailer");
+			nodemailer  = require("nodemailer"),
+			request     = require("request");
 
 app.use(express.static('public'));
 mongoose.connect("mongodb://localhost/TShair");
@@ -13,12 +14,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // MONGOOSE SCHEMA
 //===============================================
 
-var contactEmailSchema = new mongoose.Schema({
-    queryEmail: String
+var queryContactEmailSchema = new mongoose.Schema({
+    email: String
 });
 
-var contactEmail = mongoose.model("TShairEmail", contactEmailSchema);
-
+var queryContactEmail = mongoose.model("TShairEmail", queryContactEmailSchema);
 
 //===============================================
 // ROUTES
@@ -28,6 +28,23 @@ app.get("/", function(req, res){
     res.render("36hair");
 });
 
+app.post("/", function(req, res){
+
+	// get form info that was submitted
+	let email = req.body.mailingEmail;
+	let newEmail = {email:email};
+
+	// add email address to data base
+	queryContactEmail.create(newEmail, function(err){
+			if(err){
+					console.log(err);
+			}
+	});
+
+	// redirect to contact page with "message sent alert"
+	res.redirect('/');
+});
+
 app.get("/contact", function(req, res){
 	res.render("contact");
 });
@@ -35,13 +52,13 @@ app.get("/contact", function(req, res){
 app.post("/contact", function(req, res){
 
 		// get form info that was submitted
-		let queryEmail = req.body.queryEmail;
+		let email = req.body.queryEmail;
     let queryText = req.body.queryText;
-    let newEmail = {queryEmail:queryEmail};
+    let newEmail = {email:email};
 
 		// check email address isnt already in the database
 		// add email address to data base
-		contactEmail.create(newEmail, function(err){
+		queryContactEmail.create(newEmail, function(err){
         if(err){
             console.log(err);
         }
@@ -63,8 +80,8 @@ app.post("/contact", function(req, res){
 		let HelperOptions = {
 			from: '"Ben Hall" <Benjamin.hall2442@gmail.com>',
 			to: 'Benjamin.hall2442@gmail.com',
-			subject: queryEmail,
-			text: "<"+queryEmail+">"+ queryText
+			subject: email,
+			text: "<"+email+">"+ queryText
 		};
 		transporter.sendMail(HelperOptions,(error, info) => {
 			if(error){
